b9c626692ff0ee9025b6263869e9957200f0d8a5
KimJinHo6831 <52987098+KimJinHo6831@users.noreply.github.com>
2021-10-06 11:58:27 +0900

    Create-react-app and make page, css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [value, setValue] = useState(5);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <canvas className="canvas"></canvas>
+
+      <div className="controls">
+        <div className="controls__range">
+          <input
+            type="range"
+            className="jsRange"
+            min="1"
+            max="10"
+            value={value}
+            onChange={handleChange}
+            step="1"
+          />
+        </div>
+
+        <div className="controls__btns">
+          <button className="jsMode">Fill</button>
+          <button className="jsSave">Save</button>
+          <button className="jsMode">Fill2</button>
+        </div>
+
+        <div className="controls__colors">
+          <div
+            className="controls__color"
+            style={{ backgroundColor: "black" }}
+          ></div>
+          <div
+            className="controls__color"
+            style={{ backgroundColor: "white" }}
+          ></div>
+          <div
+            className="controls__color"
+            style={{ backgroundColor: "#FF3B30" }}
+          ></div>
+          <div
+            className="controls__color"
+            style={{ backgroundColor: "#FF9500" }}
+          ></div>
+          <div
+            className="controls__color"
+            style={{ backgroundColor: "#FFCC00" }}
+          ></div>
+          <div
+            className="controls__color"
+            style={{ backgroundColor: "#4CD963" }}
+          ></div>
+          <div
+            className="controls__color"
+            style={{ backgroundColor: "#5AC8FA" }}
+          ></div>
+          <div
+            className="controls__color"
+            style={{ backgroundColor: "#0579FF" }}
+          ></div>
+          <div
+            className="controls__color"
+            style={{ backgroundColor: "#5856D6" }}
+          ></div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
